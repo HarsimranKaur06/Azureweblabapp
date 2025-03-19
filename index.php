81b82a26b6870b2ccb372feccb6ddb3843789dac
Harsimran Kaur <harsimrankaur24@loyalistcollege.com>
2025-03-18 21:42:21 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Information</title>
+</head>
+<body>
+    <table border="1" align="center">
+        <tr>
+            <th>Student ID</th>
+            <th>Name</th>
+            <th>Course Code</th>
+        </tr>
+        <tr>
+            <td>500234966</td>
+            <td>Harsimran Kaur</td>
+            <td>CLOD2005</td>
+        </tr>
+    </table>
+</body>
+</html>
